df620f4dd4471ce5339ea58f0f6947d2d9e6b248
sophiedown <sophiedown@riccarton.school.nz>
2024-07-04 09:23:14 +1200

    Make user interface

--- py.py
+++ py.py
@@ -29,7 +29,13 @@ while menu_choice != 'Z':
                         'A: All information\n'
                         'B: Top 10 most played songs\n'
                         'C: Songs longer than 3 minutes\n'
-                        'D: All songs lsited newst to oldest\n'
+                        'D: All songs listed newest to oldest\n'
+                        'E: Unpopular old songs\n'
+                        'F: Songs that are part of an album\n'
+                        'G: Popular new songs\n'
+                        'H: Songs listed most liked to least liked on youtube\n'
+                        'I: Songs between 3 and 4 minutes long\n'
+                        'J: Popular songs by Australian artists\n'
                         'Z: Exit\n\nType option here: ')
     menu_choice = menu_choice.upper()
     if menu_choice == 'A':
@@ -40,3 +46,15 @@ while menu_choice != 'Z':
         print_query('long songs')
     elif menu_choice == 'D':
         print_query('newest to oldest')
+    elif menu_choice == 'E':
+        print_query('least popular old songs')
+    elif menu_choice == 'F':
+        print_query('songs from albums')
+    elif menu_choice == 'G':
+        print_query('popular new songs')
+    elif menu_choice == 'H':
+        print_query('most liked to least liked')
+    elif menu_choice == 'I':
+        print_query('good length songs')
+    elif menu_choice == 'J':
+        print_query('aussie popular songs')
